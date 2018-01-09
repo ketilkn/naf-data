cef2e23004c052c2aa566d60c2af3e00d40585ff
Ketil Nordstad <ketilkn@scan4news.com>
2018-01-09 16:44:26 +0100

    Download recent tournaments

--- nafstat/update.py
+++ nafstat/update.py
@@ -2,15 +2,36 @@
 """ Update nafstat from members.thenaf.net """
 import sys
 import logging
-
-import session
+import nafstat.tournament.tournamentlist
+import nafstat.tournament.fetch_tournament
+import nafstat.tournament.fetch_tournamentmatch
 
 LOG = logging.getLogger(__package__)
+
+
+def update():
+    LOG.info("Updating NAF data")
+    recent_tournaments = list(
+        nafstat.tournament.tournamentlist.recent(
+            nafstat.tournament.tournamentlist.list_tournaments()
+        ))
+
+    LOG.debug("%s recent tournaments", len(recent_tournaments))
+    for t in recent_tournaments:
+        LOG.info("Tournament %s", nafstat.tournament.tournamentlist.tournament_line(t))
+        LOG.debug("Downloading tournament data")
+        nafstat.tournament.fetch_tournament.fetch_tournament(t["tournament_id"])
+        LOG.debug("Downloading tournament matches")
+        nafstat.tournament.fetch_tournamentmatch.fetch_tournamentmatch(t["tournament_id"])
+
+
 
 
 def main():
     log_format = "[%(levelname)s:%(filename)s:%(lineno)s - %(funcName)20s ] %(message)s"
     logging.basicConfig(level=logging.DEBUG, format=log_format)
+    update()
+
 
 
 if __name__ == "__main__":
