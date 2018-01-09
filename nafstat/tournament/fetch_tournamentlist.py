8d8eb43e7f45369c9fa38198c4ee4e5579755ef0
Ketil Nordstad <ketilkn@scan4news.com>
2018-01-09 13:09:41 +0100

    Add option to return downloaded file

--- nafstat/tournament/fetch_tournamentlist.py
+++ nafstat/tournament/fetch_tournamentlist.py
@@ -13,10 +13,10 @@ DEFAULT_TARGET = "data/naf_tourneys.html"
 
 
 def fetch_tournamentlist(url = TOURNAMENT_LIST_URL, target = DEFAULT_TARGET):
-    return fetch_url(url, target)
+    return fetch_url(url, target, True)
 
 
-def fetch_url(url, target):
+def fetch_url(url, target, returnFile = False):
     if not url:
         LOG.error("url is %s", url)
         sys.exit(1)
@@ -39,16 +39,21 @@ def fetch_url(url, target):
     download_session = nafstat.session.new_session()
     result = nafstat.session.download_to(download_session, url, target)
     LOG.debug("download_to returned %s in %s ms", result, round((time.time()-start_time)*1000))
+
+    if returnFile:
+        with open(target, "rb") as target_file:
+            return target_file.read().decode(encoding="UTF-8")
+
     return result
-
-
 
 
 def main():
     log_format = "[%(levelname)s:%(filename)s:%(lineno)s - %(funcName)20s ] %(message)s"
     logging.basicConfig(level=logging.DEBUG, format=log_format)
 
-    fetch_tournamentlist(TOURNAMENT_LIST_URL, DEFAULT_TARGET)
+    result = fetch_tournamentlist(TOURNAMENT_LIST_URL, DEFAULT_TARGET)
+
+    print(result)
 
 
 if __name__ == "__main__":
