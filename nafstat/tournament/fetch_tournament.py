4b0681b8fb87ea9245da66dc58484aa802f9cc9a
Ketil Nordstad <ketilkn@scan4news.com>
2018-01-09 15:30:05 +0100

    Fetch tournament data

--- nafstat/tournament/fetch_tournament.py
+++ nafstat/tournament/fetch_tournament.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 """  Parse match from HTML """
-import sys
 import logging
+from . import fetch_tournamentlist
 
 LOG = logging.getLogger(__package__)
+DEFAULT_TARGET = "data/tournaments/t{}.html"
+TOURNAMENT_MATCH_URL = "https://member.thenaf.net/index.php?module=NAF&type=view&id={}&advanced=1"
 TOURNAMENT_URL = "https://member.thenaf.net/index.php?module=NAF&type=tournaments&func=view&id={}"
 
+
+def fetch_tournament(tournament_id, url = TOURNAMENT_URL, target = DEFAULT_TARGET):
+    LOG.debug("Fetch tournament %s", tournament_id)
+    return fetch_tournamentlist.fetch_url(url.format(tournament_id), target.format(tournament_id), True)
 
 
 def main():
     log_format = "[%(levelname)s:%(filename)s:%(lineno)s - %(funcName)20s ] %(message)s"
     logging.basicConfig(level=logging.DEBUG, format=log_format)
+    data_for_tournament = fetch_tournament("3154")
+    print(data_for_tournament)
 
 
 if __name__ == "__main__":
