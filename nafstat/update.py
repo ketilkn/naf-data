551a55f4621efc6bdb1c585a3664bb75c232db0c
Ketil Nordstad <ketilkn@scan4news.com>
2018-03-23 21:23:22 +0100

    Use humandriendly for time output

--- nafstat/update.py
+++ nafstat/update.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 """ Update nafstat from members.thenaf.net """
 import time
+import datetime
 import logging
 import logging.config
+import humanfriendly
 from nafstat.tournament import tournamentlist
 import nafstat.tournament.fetch_tournament
 import nafstat.tournament.fetch_tournamentmatch
@@ -11,27 +13,27 @@ logging.config.fileConfig('pylogging.conf')
 LOG = logging.getLogger("nafstat.update")
 
 
-def download(downloader, t, throttle=True):
+def download(downloader, tournament, throttle=True):
     start_time = time.time()
-    LOG.debug("Using downloader %s with id %s", downloader.__name__, t)
-    downloader(t["tournament_id"])
+    LOG.debug("Using downloader %s with id %s", downloader.__name__, tournament["tournament_id"])
+    downloader(tournament["tournament_id"])
     request_time = time.time()-start_time
 
     if throttle:
-        LOG.debug("Waiting %ss", request_time)
+        LOG.debug("Waiting %s", humanfriendly.format_timespan(request_time))
         time.sleep(request_time)
     else:
         LOG.debug("Throttle is False")
 
 
-def update_tournament(t, throttle=True):
-    LOG.info("Tournament %s", nafstat.tournament.tournamentlist.tournament_line(t))
+def update_tournament(tournament, throttle=True):
+    LOG.info("Tournament %s", nafstat.tournament.tournamentlist.tournament_line(tournament))
 
     LOG.debug("Downloading tournament data")
-    download(nafstat.tournament.fetch_tournament.fetch_tournament, t, throttle)
+    download(nafstat.tournament.fetch_tournament.fetch_tournament, tournament, throttle)
 
     LOG.debug("Downloading tournament matches")
-    download(nafstat.tournament.fetch_tournamentmatch.fetch_tournamentmatch, t, throttle)
+    download(nafstat.tournament.fetch_tournamentmatch.fetch_tournamentmatch, tournament, throttle)
 
 
 def update_list(tournaments, throttle=True):
@@ -59,7 +61,10 @@ def update(throttle=True):
 
 def main():
     import sys
+    LOG.info("Update stared at %s", datetime.datetime.now().isoformat())
+    start = time.time()
     update(throttle="--no-throttle" not in sys.argv)
+    LOG.info("Update ended after %s at %s", humanfriendly.format_timespan(time.time() - start), datetime.datetime.now().isoformat())
 
 
 if __name__ == "__main__":
