c18924d7a99fea9a44d004d8b708a8f8fb8b6749
Ketil Nordstad <ketilkn@scan4news.com>
2018-03-23 19:10:39 +0100

    Merge repeated code

--- nafstat/update.py
+++ nafstat/update.py
@@ -9,52 +9,57 @@ import nafstat.tournament.fetch_tournamentmatch
 LOG = logging.getLogger(__package__)
 
 
-def update_tournament(t, throttle = True):
-    LOG.info("Tournament %s", nafstat.tournament.tournamentlist.tournament_line(t))
-    LOG.debug("Downloading tournament data")
-
-    tournament_start = time.time()
-    nafstat.tournament.fetch_tournament.fetch_tournament(t["tournament_id"])
-    tournament_wait = time.time()-tournament_start
+def download(downloader, t, throttle=True):
+    start_time = time.time()
+    LOG.debug("Using downloader %s with id %s", downloader.__name__, t)
+    downloader(t["tournament_id"])
+    request_time = time.time()-start_time
 
     if throttle:
-        LOG.debug("Waiting %ss", tournament_wait)
-        time.sleep(tournament_wait)
+        LOG.debug("Waiting %ss", request_time)
+        time.sleep(request_time)
+    else:
+        LOG.debug("Throttle is False")
+
+
+def update_tournament(t, throttle=True):
+    LOG.info("Tournament %s", nafstat.tournament.tournamentlist.tournament_line(t))
+
+    LOG.debug("Downloading tournament data")
+    download(nafstat.tournament.fetch_tournament.fetch_tournament, t, throttle)
 
     LOG.debug("Downloading tournament matches")
-
-    matches_start = time.time()
-    nafstat.tournament.fetch_tournamentmatch.fetch_tournamentmatch(t["tournament_id"])
-    matches_wait = time.time()-matches_start
-    if throttle:
-        LOG.debug("Waiting %ss", matches_wait)
-        time.sleep(matches_wait)
+    download(nafstat.tournament.fetch_tournamentmatch.fetch_tournamentmatch, t, throttle)
 
 
-def update_list(tournaments):
+def update_list(tournaments, throttle=True):
     LOG.debug("%s recent tournaments", len(tournaments))
     for idx, t in enumerate(tournaments):
-        update_tournament(t)
+        update_tournament(t, throttle)
 
 
-def update():
+def update(throttle=True):
     """ Download recent tournaments from thenaf.net  """
     LOG.info("Updating NAF data")
+    if not throttle:
+        LOG.warning("No delay between requests")
+    else:
+        LOG.debug("Politely using delay between requests")
 
     recent_tournaments = list(tournamentlist.recent(tournamentlist.list_tournaments()))
     LOG.info("{} recent tournament{}".format(len(recent_tournaments), "s" if len(recent_tournaments) != 1 else ""))
-    update_list(recent_tournaments[0:5])
+    update_list(recent_tournaments, throttle)
 
     new_tournaments = list(tournamentlist.no_data(tournamentlist.list_tournaments()))
     LOG.info("{} new tournament{}".format(len(new_tournaments), "s" if len(new_tournaments) != 1 else ""))
-    update_list(new_tournaments)
+    update_list(new_tournaments, throttle)
 
 
 def main():
     import sys
     log_format = "[%(levelname)s:%(filename)s:%(lineno)s - %(funcName)20s ] %(message)s"
     logging.basicConfig(level=logging.DEBUG if "--DEBUG" in sys.argv else logging.INFO, format=log_format)
-    update()
+    update(throttle="--no-throttle" not in sys.argv)
 
 
 if __name__ == "__main__":
