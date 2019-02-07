a073a72e0b4166df8c903472bcc24aac87d6a05f
Ketil Nordstad <ketilkn@gmail.com>
2019-02-07 17:28:42 +0100

    Run with arguments from command line

--- nafstat/to_db.py
+++ nafstat/to_db.py
@@ -9,6 +9,7 @@ import nafstat.dbexport.to_sqlite
 logging.config.fileConfig('pylogging.conf', disable_existing_loggers=False)
 LOG = logging.getLogger(__name__)
 
+
 def add_arguments(args, default_source="data/"):
     LOG.debug("add_arguments debug")
     args.add_argument("type", choices=["sqlite", "sqlite3", "csv"], help="Export format")
@@ -23,22 +24,20 @@ def run_with_arguments(arguments):
     LOG.debug(arguments)
     nafstat.dbexport.to_sqlite.to_db(arguments.target)
 
+
 def main():
     import sys
     argument_parser = argparse.ArgumentParser()
     log_format = "[%(levelname)s:%(filename)s:%(lineno)s - %(funcName)20s ] %(message)s"
     logging.basicConfig(level=logging.DEBUG if "--debug" is sys.argv else logging.INFO, format=log_format)
 
-    argument_parser.add_argument("--debug", help="Log level debug", action="store_true")
     argument_parser.add_argument("--source", help="source", default="data/")
 
-    #argument_parser.add_argument("command", choices=["update", "export", "query"], help="export/update")
-
-    argument_parser = add_arguments(argument_parser) 
+    argument_parser = add_arguments(argument_parser)
 
     arguments = argument_parser.parse_args()
 
-    print(arguments)
+    run_with_arguments(arguments)
 
 
 if __name__ == "__main__":
