ee993065f5c3c1b806091a7cfcd2795fd9f345dc
Ketil Nordstad <ketilkn@gmail.com>
2019-10-16 23:53:57 +0200

    Add documentation

--- nafparser/__init__.py
+++ nafparser/__init__.py
@@ -23,30 +23,31 @@ def _file_to_soup(filename: str) -> bs4.BeautifulSoup:
 
 
 def parse_coach(source: str) -> typing.Dict:
-    """Parse source (filename) as coachpage. Return dict of coach"""
+    """Parse source:str (filename) as coachpage. Return dict of coach"""
     LOG.debug('Parsing coach from %s', type(source))
     return nafparser.coachparser.fromfile(source)
 
 
 def parse_tournament(source: str) -> typing.Dict:
-    """Parse source (filename) as tournament. Return dict of tournament"""
+    """Parse source:str (filename) as tournament. Return dict of tournament"""
     LOG.debug('Parsing tournament from %s', type(source))
     return nafparser.tournamentparser.parse_tournament(_file_to_soup(source))
 
 
 def parse_tournaments(source: str) -> typing.List[typing.Dict]:
-    """Parse source (filename) as tournament list. Return list of limited tournament dicts"""
+    """Parse source:str (filename) as tournament list. Return list of limited tournament dicts"""
     LOG.debug('Parsing tournaments from %s', type(source))
     return nafparser.tournamentlistparser.load2(nafparser.tournamentlistparser.parse_file, filename=source)
 
 
 def parse_tournamentmatches(source: str) -> typing.List[typing.Dict]:
-    """Parse source (filename) as tournament matches. Return List with dicts of matches"""
+    """Parse source:str (filename) as tournament matches. Return List with dicts of matches"""
     LOG.debug('Parsing tournament matches from %s', type(source))
     return nafparser.matchesparser.from_file(source)
 
 
 def parse_auto(source: str):
+    """Attempts to select the most appropriate parser for the provided source:str"""
     LOG.debug('Parsing auto from %s', type(source))
     if os.path.isfile(source):
         if 'coach' in source:
@@ -60,10 +61,8 @@ def parse_auto(source: str):
 
 
 def parse(source, parser=parse_auto):
-    """Accept source or list of sources. Yields values returned from parser."""
+    """Accept source:str or list of source:List[str]. *Yield* values returned from parser."""
     sources = source if isinstance(source, list) else [source]
-
-
     for s in sources:
         yield parser(s)
 
