0c021a96a1139c7325d5922aa30ca4e1800081cb
Ketil Nordstad <ketilkn@gmail.com>
2023-07-10 23:35:19 +0200

    Initial implementation of  mysql tournament loader

---
+++ nafstat/dbloader.py
@@ -0,0 +1,105 @@
+import configparser
+import datetime
+import pathlib
+
+import pymysql
+import rich
+
+
+TOURNAMENT_QUERY = """
+SELECT nt.tournamentname, nt.tournamentid, nt.tournamentstartdate, nt.tournamentenddate,
+    ntv.variantname, nt.tournamentstyle, nt.tournamentscoring, nt.tournamentorg, 
+    nt.tournamentemail, nt.tournamenturl, nt.tournamentinformation,
+    nt.tournamentnation, nt.tournamentcity, 
+    nts.winnerCoachID, nts.runnerUpCoachID, nts.mostTouchdownsCoachID, nts.mostCasualitiesCoachID, nts.stuntyCupCoachID,
+    nts.bestPainterCoachID,
+    ntr.rulesetname
+FROM naf_tournament nt
+LEFT JOIN naf_tournament_statistics nts ON nts.tournamentID=nt.tournamentid
+LEFT JOIN naf_variants ntv ON ntv.variantid = nt.naf_variantsid
+LEFT JOIN naf_ruleset ntr ON ntr.rulesetid = nt.naf_rulesetid
+"""
+
+
+def _locate_naf_ini(requested_file):
+    if requested_file and requested_file != 'naf.ini':
+        return pathlib.Path(requested_file)
+    here = pathlib.PosixPath('naf.ini')
+    if here.exists():
+        return here
+    module_parent = pathlib.PosixPath(__file__).parent.parent / 'naf.ini'
+    return module_parent
+
+
+def load_config(config_file='naf.ini', section='nafdata.mysql') -> dict:
+    """Load configuration from ini file and return dict"""
+    filename = pathlib.PosixPath(_locate_naf_ini(config_file))
+
+    if not filename.exists():
+        raise FileNotFoundError('Unable to locate {}'.format(config_file if config_file else filename))
+
+    config = configparser.ConfigParser()
+    config.read(filename)
+
+    return config[section]
+
+
+def create_connection():
+    config = load_config()
+    connection = pymysql.connect(host=config.get('host'),
+                                 user=config.get('user'),
+                                 password=config.get('password'),
+                                 database=config.get('database'),
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    return connection
+
+
+def load_tournaments(connection=None):
+    con = connection or create_connection()
+    with con.cursor() as cursor:
+        query = 'SELECT * FROM naf_tournament'
+        cursor.execute(TOURNAMENT_QUERY)
+        for row in cursor.fetchall():
+            start_date = row.get('tournamentenddate')
+            end_date = row.get('tournamentenddate')
+            tournament = {
+                'name': row.get('tournamentname'),
+                'tournament_id': row.get('tournamentid'),
+                'location': row.get('tournamentnation'),
+                'start_date': start_date,
+                'end_date': end_date,
+                'variant': row.get('variantname'),
+                'style': row.get('tournamentstyle'),
+                'scoring': row.get('tournamentscoring'),
+                'type': row.get('tournamenttype'),
+                'organizer': row.get('tournamentorg'),
+                'email': row.get('tournamentemail'),
+                'webpage': row.get('tournamenturl'),
+                'information': row.get('tournamentinformation'),
+                'nation': row.get('tournamentnation'),
+                'city': row.get('tournamentcity'),
+                '_last_updated': datetime.datetime.now().isoformat(),
+                'awards': {'winner': row.get('winnerCoachID'),
+                           'runner up': row.get('runnerUpCoachID'),
+                           'most touchdowns': row.get('mostTouchdownsCoachID'),
+                           'most casualties': row.get('mostCasualitiesCoachID'),
+                           'stunty cup': row.get('stuntyCupCoachID'),
+                           'best painted': row.get('bestPainterRaceID')},
+                'other_awards': 'N/A',
+                'matches': 'N/A',
+                'swiss': 'N/A',
+                'match_count': 'N/A',
+                'casualties': 'N/A',
+                'touchdowns': 'N/A',
+                'ruleset': row.get('rulesetname')}
+            yield tournament
+
+
+def main():
+    for tournament in load_tournaments():
+        rich.print(tournament)
+
+
+if __name__ == '__main__':
+    main()
